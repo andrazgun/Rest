23271143c6c40b976d3ff2b0f8019cb13b7929af
Andrei Gunta <101417850+andrazgun@users.noreply.github.com>
2025-05-26 13:30:56 +0300

    Additional tests (#3)

--- src/test/java/com/rest/DynamicJson.java
+++ src/test/java/com/rest/DynamicJson.java
@@ -1,5 +1,7 @@
 import io.restassured.RestAssured;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import utils.ConfigLoader;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,9 +12,81 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DynamicJson {
 
+    @DataProvider(name = "bookData")
+    public Object[][] getBookData() {
+        return new Object[][]{
+                {"abc", "1234", "Test Automation", "Andrei"},
+                {"bcd", "5678", "API Testing", "John Doe"},
+                {"xyz", "9012", "DevOps Handbook", "Jane Smith"}
+        };
+    }
+
+    @Test(dataProvider = "bookData")
+    public void addBook(String isbn, String aisle, String bookName, String author) {
+        RestAssured.baseURI = ConfigLoader.get("baseURI");
+
+        String requestBody = """
+            {
+              "name": "%s",
+              "isbn": "%s",
+              "aisle": "%s",
+              "author": "%s"
+            }
+            """.formatted(bookName, isbn, aisle, author);
+
+        String responseBody =
+                given()
+                        .log().all()
+                        .header("Content-Type", "application/json")
+                        .body(requestBody)
+                        .when()
+                        .post("/Library/Addbook.php")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract().response().asString();
+
+        System.out.println("AddBook Response: " + responseBody);
+    }
+
+    @DataProvider(name = "bookIdData")
+    public Object[][] getBookIdData() {
+        return new Object[][]{
+                {"abc", "1234"},
+                {"bcd", "5678"},
+                {"xyz", "9012"}
+        };
+    }
+
+    @Test(dataProvider = "bookIdData")
+    public void deleteBook(String isbn, String aisle) {
+        RestAssured.baseURI = ConfigLoader.get("baseURI");
+
+        String bookId = isbn + aisle;
+        String requestBody = """
+        {
+          "ID": "%s"
+        }
+        """.formatted(bookId);
+
+        String responseBody =
+                given()
+                        .log().all()
+                        .header("Content-Type", "application/json")
+                        .body(requestBody)
+                        .when()
+                        .delete("/Library/DeleteBook.php")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract().response().asString();
+
+        System.out.println("DeleteBook Response: " + responseBody);
+    }
+
     @Test
-    public void addBook() throws IOException {
-        RestAssured.baseURI = "https://rahulshettyacademy.com/";
+    public void addBook1() throws IOException {
+        RestAssured.baseURI = ConfigLoader.get("baseURI");
 
         String filePath = "src/main/resources/Library/addBook.json";
         assertThat(Files.exists(Paths.get(filePath)))
@@ -38,7 +112,7 @@ public class DynamicJson {
 
     @Test
     public void getBookById() {
-        RestAssured.baseURI = "https://rahulshettyacademy.com/";
+        RestAssured.baseURI = ConfigLoader.get("baseURI");
         String bookId = "bcd2926";
 
         String response =
